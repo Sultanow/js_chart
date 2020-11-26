d54440e327ff6bb38b831c2e13b584a86eb58f78
Sultanow <sultanow@outlook.com>
2020-11-26 17:43:57 +0100

    finalized/fixed graph drawing

--- app-chart/src/app/app.component.ts
+++ app-chart/src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as d3 from 'd3';
 
+/* 
+ * Helpful links:
+ * https://ialab.it.monash.edu/webcola/examples/unix.html
+ * https://twitter.com/johndavidfive/status/814720460566843392
+ * http://bl.ocks.org/fancellu/2c782394602a93921faff74e594d1bb1
+ * https://observablehq.com/@xianwu/force-directed-graph-network-graph-with-arrowheads-and-lab
+ * http://bl.ocks.org/mbostock/2706022
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,70 +27,12 @@ export class AppComponent implements OnInit {
   private root = {};
   private dataBatches = new Map();
   private dataBatchGraph = [];
-  private treeData1 = {};
 
-  /*
-  private treeData = {
-    "name": "Eve",
-    "value": 15,
-    "type": "black",
-    "level": "yellow",
-    "children": [
-      {
-        "name": "Cain",
-        "value": 10,
-        "type": "grey",
-        "level": "red"
-      },
-      {
-        "name": "Seth",
-        "value": 10,
-        "type": "grey",
-        "level": "red",
-        "children": [
-          {
-            "name": "Enos",
-            "value": 7.5,
-            "type": "grey",
-            "level": "purple"
-          },
-          {
-            "name": "Noam",
-            "value": 7.5,
-            "type": "grey",
-            "level": "purple"
-          }
-        ]
-      },
-      {
-        "name": "Abel",
-        "value": 10,
-        "type": "grey",
-        "level": "blue"
-      },
-      {
-        "name": "Awan",
-        "value": 10,
-        "type": "grey",
-        "level": "green",
-        "children": [
-          {
-            "name": "Enoch",
-            "value": 7.5,
-            "type": "grey",
-            "level": "orange"
-          }
-        ]
-      },
-      {
-        "name": "Azura",
-        "value": 10,
-        "type": "grey",
-        "level": "green"
-      }
-    ]
+  private treeData = {};
+  private graphData = {
+    nodes: [],
+    links: []
   };
-  */
 
   private margin = { top: 20, right: 120, bottom: 20, left: 120 };
   private width = 960 - this.margin.right - this.margin.left;
@@ -90,11 +41,18 @@ export class AppComponent implements OnInit {
   private svg;
 
   async ngOnInit() {
+    this.svg = d3.select("svg#graph")
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom);
+
     this.dataBatches = await this.getNodes();
     this.dataBatchGraph = await this.getEdges();
-    console.log("tree is");
-    console.log(this.treeData1);
-    this.constructTree(this.root, this.treeData1);
+    //this.constructTree(this.root, this.treeData);
+    //this.drawTree(this.treeData);
+
+    this.constructGraph(this.dataBatches, this.dataBatchGraph);
+    console.log(this.graphData);
+    this.drawGraph(this.graphData);
   }
 
   private async getNodes() {
@@ -122,33 +80,13 @@ export class AppComponent implements OnInit {
     let edges = [];
     list.forEach((e, i) => {
       if (i > 0) {
-        var parts = e.split(',');
-        var edge = { predecessor: parts[0], successor: parts[1] };
-        edges.push(edge);
+        var parts = e.split(','); if (parts.length > 1) {
+          var edge = { predecessor: parts[0].trim(), successor: parts[1].trim() };
+          edges.push(edge);
+        }
       }
     });
     return edges;
-  }
-
-  private constructTree(nodeSrc: any, nodeTgt: any) {
-    if (typeof nodeSrc !== 'undefined') {
-      nodeTgt.name = nodeTgt.shortName;
-      nodeTgt.value = 15;
-      nodeTgt.type = "black";
-      nodeTgt.level = "orange";
-
-      let successors = this.searchSuccessors(nodeSrc.id, this.dataBatchGraph);
-      //console.log(nodeSrc.id);
-      if (successors.length > 0) {
-        nodeTgt.children = [];
-        successors.forEach((e, i) => {
-          let successor = this.dataBatches.get(e);
-          nodeTgt.children.push(successor);
-          this.constructTree(successor, nodeTgt.children[i]);          
-        });
-      }
-      this.drawTree(this.treeData1);
-    }
   }
 
   private searchSuccessors(id: String, edges: []) {
@@ -161,17 +99,117 @@ export class AppComponent implements OnInit {
     return result;
   }
 
+  /*
+   * Directed Graph
+   */
+  private constructGraph(nodes: Map, edges: []) {
+    nodes.forEach((value: any, key: string) => {
+      this.graphData.nodes.push({ id: key, name: key, label: value.shortName });
+    });
+
+    edges.forEach((e) => {
+      this.graphData.links.push({ source: e.predecessor, target: e.successor, type: 'Next -->>' });
+    });
+  }
+
+  private drawGraph(data: any) {
+    var simulation = d3.forceSimulation<any, any>()
+      .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100).strength(1))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+
+    // build the arrow.
+    this.svg.append("svg:defs").selectAll("marker")
+      .data(["end"])
+      .enter().append("svg:marker")
+      .attr("id", String)
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 15)
+      .attr("refY", -1.5)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+      .append("svg:path")
+      .attr("d", "M0,-5L10,0L0,5");
+
+    var link = this.svg.append("g").selectAll(".link")
+      .attr("class", "link")
+      .data(data.links)
+      .enter()
+      .append("line")
+      .attr("stroke", "black")
+      .style("stroke-width", 2)
+      .attr("marker-end", "url(#end)");
+
+    var node = this.svg.selectAll(".node")
+      .data(data.nodes)
+      .enter()
+      .append("g")
+      .call(d3.drag()
+        .on("start", function (event, d) {
+          if (!event.active) simulation.alphaTarget(0.3).restart()
+          d.fx = d.x;
+          d.fy = d.y;
+        })
+        .on("drag", function (event, d) {
+          d.fx = event.x;
+          d.fy = event.y;
+        }));
+
+    node.append("rect")
+      .attr("width", 40)
+      .attr("height", 20)
+      .attr("transform", function (d) { return "translate(-20,-10)" })
+      .style("fill", "lightgray");
+
+    node.append("text")
+      .attr("x", -20)
+      .attr("dy", ".2em")
+      .text(function (d) { return d.label });
+
+    var ticked = function () {
+      link
+        .attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+      node
+        .attr("transform", function (d) { return "translate(" + d.x + ", " + d.y + ")"; });
+    }
+    simulation.nodes(data.nodes).on("tick", ticked);
+    simulation.nodes(data.nodes);
+    simulation.force<d3.ForceLink<any, any>>("link").links(data.links);
+  }
+
+  /*
+   * Tree
+   */
+  private constructTree(nodeSrc: any, nodeTgt: any) {
+    if (typeof nodeSrc !== 'undefined') {
+      nodeTgt.name = nodeSrc.shortName;
+      nodeTgt.value = 15;
+      nodeTgt.type = "black";
+      nodeTgt.level = "orange";
+
+      let successors = this.searchSuccessors(nodeSrc.id, this.dataBatchGraph);
+      if (successors.length > 0) {
+        nodeTgt.children = [];
+        successors.forEach((e, i) => {
+          let successor = this.dataBatches.get(e);
+          nodeTgt.children.push(successor);
+          this.constructTree(successor, nodeTgt.children[i]);
+        });
+      }
+    }
+  }
+
   private drawTree(data: any) {
     const treemap = d3.tree().size([this.height, this.width]);
     console.log(data);
     let nodes = d3.hierarchy(data, d => d.children);
     nodes = treemap(nodes);
-
-    const svg = d3.select("svg#graph")
-      .attr("width", this.width + this.margin.left + this.margin.right)
-      .attr("height", this.height + this.margin.top + this.margin.bottom);
-
-    const g = svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    const g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
     const link = g.selectAll(".link").data(nodes.descendants().slice(1)).enter().append("path")
       .attr("class", "link")
